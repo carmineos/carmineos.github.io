7d57529dc7181143090383f64dcfc98be646e7e8
Carmine Giugliano <neos7@live.it>
2020-09-19 11:17:22 +0200

    added source

---
+++ BlazorApp/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp.Models
+{
+    public class Movie
+    {
+        public string OriginalTitle { get; set; }
+        public string TitleIT { get; set; }
+        public int Year { get; set; }
+        public string TitleId { get; set; }
+        public string Directors { get; set; }
+    }
+}
---
+++ BlazorApp/Models/MoviesList.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp.Models
+{
+    public class MoviesList
+    {
+        public string Name { get; set; }
+
+        public Movie[] Movies { get; set; }
+    }
+}
