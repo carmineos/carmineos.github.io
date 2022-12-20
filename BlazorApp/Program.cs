834ddd569c066ef9d1a07b97d9b480ff2df67089
carmineos <neos7@live.it>
2022-12-21 00:52:10 +0100

    update template to .NET7

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,6 +1,6 @@
-using BlazorApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
